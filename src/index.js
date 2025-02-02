07500749251a4469376e0ccb9d4a053b17488d4e
Emílio Silva <emiliosarges@gmail.com>
2025-02-01 21:56:10 -0300

    up

--- src/index.js
+++ src/index.js
@@ -6,3 +6,23 @@ const link = caminhoArquivo[2];
 fs.readFile(link, 'utf-8', (erro, texto) => {
     console.log(texto);
 })
+
+//Lista de Tarefas
+//Criar um array com as palavras
+//contar as ocorrências
+//montar um objeto com o resultado
+
+// {
+//     "web": 5,
+//     "computador": 4
+// }
+
+function vericaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra]) || 0) +1;
+    })
+    console.log(resultado); 
+}
