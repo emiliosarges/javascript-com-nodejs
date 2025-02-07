860f0ac672e611a81436b1f9e307c186f29336c3
Emílio Silva <emiliosarges@gmail.com>
2025-02-06 22:39:37 -0300

    up

---
+++ src/cli.js
@@ -0,0 +1,29 @@
+import { readFile } from 'fs';
+import trataErros from './erros/funcoesErro.js';
+import { contaPalavras } from './index.js';
+
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3]
+
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    try {
+        if (erro) throw erro
+        const resultado = contaPalavras(texto);
+        criaESalvaArquivo(resultado, endereco);
+    }   catch(erro) {
+       console.log(trataErros(erro));
+    }
+})
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+    const arquivoNovo = `${endereco}/resultado.txt`;
+    const textoPalavras = JSON.stringify(listaPalavras);
+    try {
+        await fs.promises.writeFile(arquivoNovo, textoPalavras);
+        console.log('Arquivo criado');
+    } catch(erro) {
+        throw erro;
+    }
+}
